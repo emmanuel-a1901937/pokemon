4a3b0885f61b903d9fc8260e4c47c823e25256f0
Emmanuel <emmanuel@Emmanuel-Laptop>
2024-05-10 09:55:00 +0930

    aaa

--- Move.h
+++ Move.h
@@ -1,1 +1,18 @@
-fuck you 
+#ifndef MOVE_H
+#define MOVE_H
+
+#include <string>
+using namespace std; 
+
+class Move {
+  private: 
+    string name; 
+    string type; 
+
+  public: 
+    virtual void useMove(Pokemon* p1, Pokemon* p2) {
+      
+    }
+};
+
+#endif

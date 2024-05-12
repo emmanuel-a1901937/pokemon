851a6b32ed059a888e77c65041ebded21f472b78
Emmanuel <emmanuel@Emmanuel-Laptop>
2024-05-12 12:49:15 +0930

    WaterDefendMove

--- WaterDefendMove.h
+++ WaterDefendMove.h
@@ -7,7 +7,16 @@ using namespace std;
 class WaterDefendMove : public DefendMove {
   private: 
     void useMove(Pokemon* p1, Pokemon* p2) {
-      // p2 is not necessary since DefendMove is used on itself 
+      // Note: p2 is not used since DefendMove is used on itself 
+
+      // apply new attributes to p1 
+      int newAttack = p1->getAttack()  + this->attackEffect; // calculate new attributes 
+      int newDefence = p1->getDefence() + this->defenceEffect; 
+      int newSpeed = p1->getSpeed() + this->speedEffect; 
+
+      p1->setAttack(newAttack); // set new attributes
+      p1->setDefence(newDefence);
+      p1->setSpeed(newSpeed); 
     }
 };
 

a89262e283dc208f1af6fadb597c4d9416b2c34e
Emmanuel <emmanuel@Emmanuel-Laptop>
2024-05-17 10:05:59 +0930

    defend moves

--- FirePokemon.cpp
+++ FirePokemon.cpp
@@ -39,7 +39,22 @@ FirePokemon::FirePokemon(string names) {
     possibleAttacks.push_back(a10);
 
     // possible defence
-    
+    DefendMove* d1 = new DefendMove("SharpenBlade", "Fire", 30, 0, 0); // raises attack 
+    DefendMove* d2 = new DefendMove("RaiseShield", "Fire", 0, 30 ,0); // raises defence 
+    DefendMove* d3 = new DefendMove("QuickFeet", "Fire", 0, 0, 30); // raises speed 
+    DefendMove* d4 = new DefendMove("DualWield", "Fire", 60, -40, 0); // doubles attack, halves defence 
+    DefendMove* d5 = new DefendMove("DualShield", "Fire", -40, 60, 0); // doubles defence, halves attack 
+    DefendMove* d6 = new DefendMove("LightningBoots", "Fire", 0, -40, 60); // doubles speed, halves defence
+    DefendMove* d7 = new DefendMove("HeavyArmour", "Fire", 0, 60, -40); // doubles defence, halves speed
+    DefendMove* d8 = new DefendMove("GreatWeapon", "Fire", 60, 0, -40); // doubles attack, halves speed
+    possibleDefense.push_back(d1);
+    possibleDefense.push_back(d2);
+    possibleDefense.push_back(d3);
+    possibleDefense.push_back(d4);
+    possibleDefense.push_back(d5);
+    possibleDefense.push_back(d6);
+    possibleDefense.push_back(d7);
+    possibleDefense.push_back(d8);
 
     // possible status 
 
--- GrassPokemon.cpp
+++ GrassPokemon.cpp
@@ -37,7 +37,22 @@ GrassPokemon::GrassPokemon(string names) {
     possibleAttacks.push_back(a10);
 
     // possible defence
-    
+    DefendMove* d1 = new DefendMove("SharpenBlade", "Grass", 30, 0, 0); // raises attack 
+    DefendMove* d2 = new DefendMove("RaiseShield", "Grass", 0, 30 ,0); // raises defence 
+    DefendMove* d3 = new DefendMove("QuickFeet", "Grass", 0, 0, 30); // raises speed 
+    DefendMove* d4 = new DefendMove("DualWield", "Grass", 60, -40, 0); // doubles attack, halves defence 
+    DefendMove* d5 = new DefendMove("DualShield", "Grass", -40, 60, 0); // doubles defence, halves attack 
+    DefendMove* d6 = new DefendMove("LightningBoots", "Grass", 0, -40, 60); // doubles speed, halves defence
+    DefendMove* d7 = new DefendMove("HeavyArmour", "Grass", 0, 60, -40); // doubles defence, halves speed
+    DefendMove* d8 = new DefendMove("GreatWeapon", "Grass", 60, 0, -40); // doubles attack, halves speed
+    possibleDefense.push_back(d1);
+    possibleDefense.push_back(d2);
+    possibleDefense.push_back(d3);
+    possibleDefense.push_back(d4);
+    possibleDefense.push_back(d5);
+    possibleDefense.push_back(d6);
+    possibleDefense.push_back(d7);
+    possibleDefense.push_back(d8);
 
     // possible status 
 

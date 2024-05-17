c5d8772258b87faf880db1fbc7817988e64dc22a
Emmanuel <emmanuel@Emmanuel-Laptop>
2024-05-17 15:05:34 +0930

    before moving 'usemove'

--- moveTest.cpp
+++ moveTest.cpp
@@ -7,11 +7,15 @@ using namespace std;
 #include "GrassAttackMove.h"
 #include "DefendMove.h"
 #include "StatusMove.h"
+#include "Pokemon.h"
 
 int main() {
   FireAttackMove* a1 = new FireAttackMove("BlazingComet", "Fire", 20, 80);
   DefendMove* d1 = new DefendMove("SharpenBlade", "Fire", 30, 0, 0); 
   StatusMove* s1 = new StatusMove("BreakBlade", "Fire", -30, 0, 0, 90);
 
-  
+  cout << "Attack: " << a1->getName() << a1->getAccuracy() << a1->getDamage() << endl; 
+  cout << "Defend: " << d1->getName() << d1->getAttackEffect() << d1->getDefenceEffect() << d1->getSpeedEffect() << endl; 
+  cout << "Status: " << s1->getName() << s1->getAccuracy() << s1->getAttackEffect() << s1->getDefenceEffect() << s1->getSpeedEffect() << endl; 
+
 }
